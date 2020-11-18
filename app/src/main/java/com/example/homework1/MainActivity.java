0e9c03062fbaed4730a7db8d73a1adfd325e2d95
Solo4 <akasha02@mail.ru>
2020-11-18 18:36:14 +0300

    Global first commit

---
+++ app/src/main/java/com/example/homework1/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.homework1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button infoButton = findViewById(R.id.infoButton);
+
+        View.OnClickListener onClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        "EN, RU, UA, PL", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        infoButton.setOnClickListener(onClick);
+    }
+
+}
