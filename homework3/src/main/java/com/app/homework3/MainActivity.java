bc86ddcc6612685797a261a60975ea413ccca8fc
Solo4 <akasha02@mail.ru>
2020-12-02 22:41:19 +0300

    make some layout

---
+++ homework3/src/main/java/com/app/homework3/MainActivity.java
@@ -0,0 +1,18 @@
+package com.app.homework3;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout_liner_frame);
+
+
+
+    }
+}
