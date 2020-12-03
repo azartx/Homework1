4466a9cddf15b246e32f18069b3e870e27a011ff
Solo4 <akasha02@mail.ru>
2020-12-03 21:40:32 +0300

    start creating third path of homework

--- homework3/src/main/java/com/app/homework3/MainActivity.java
+++ homework3/src/main/java/com/app/homework3/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.second_layout_relative);
+        setContentView(R.layout.third_layout_linear);
 
 
 
