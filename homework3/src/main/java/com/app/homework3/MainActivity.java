336809a163d9a9c3b0b2cfe2984a7bbb27f6dd14
Solo4 <akasha02@mail.ru>
2020-12-04 23:20:47 +0300

    add 2 layouts

--- homework3/src/main/java/com/app/homework3/MainActivity.java
+++ homework3/src/main/java/com/app/homework3/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.third_layout_linear);
+        setContentView(R.layout.third_layout_constraint);
 
 
 
