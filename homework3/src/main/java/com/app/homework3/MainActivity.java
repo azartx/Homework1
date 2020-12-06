072cfbad2b6fd891f49fcf4fb6c5a5da6dd5d90f
Solo4 <akasha02@mail.ru>
2020-12-06 15:28:28 +0300

    4 layout relative some problems

--- homework3/src/main/java/com/app/homework3/MainActivity.java
+++ homework3/src/main/java/com/app/homework3/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.forth_layout_constraint);
+        setContentView(R.layout.forth_layout_relative);
 
 
 
