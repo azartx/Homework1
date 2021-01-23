a59e7a9bb8516f59ebcf02295b37b7ac7a5849d8
Solo4 <akasha02@mail.ru>
2021-01-23 19:50:02 +0300

    rxJava

--- homework5/src/main/java/com/example/homework5/database/DatabaseRepository.kt
+++ homework5/src/main/java/com/example/homework5/database/DatabaseRepository.kt
@@ -3,44 +3,45 @@ package com.example.homework5.database
 import android.content.Context
 import com.example.homework5.data.CarData
 import com.example.homework5.data.WorkData
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
-import java.util.concurrent.Executors
 
 class DatabaseRepository(context: Context) {
 
-    private val executorService = Executors.newSingleThreadExecutor()
     private val database = CarsDatabase.init(context)
 
     /*****************************
      * Start cars database block
      *****************************/
     fun addCar(car: CarData) {
-        executorService.submit { database.getCarDatabaseDAO().addCarToDatabase(car) }
+        Single.create<CarData> {
+            database.getCarDatabaseDAO().addCarToDatabase(car)
+        }.subscribeOn(Schedulers.io())
+                .subscribe()
     }
 
     fun getCarsList(): List<CarData> {
-
         return Single.create<List<CarData>> {
             val list = database.getCarDatabaseDAO().getCarsList()
             it.onSuccess(list)
         }.subscribeOn(Schedulers.io())
                 .blockingGet()
-
     }
 
 
     fun getCar(carId: Long): CarData {
-        val service = executorService.submit<CarData> {
-            return@submit database.getCarDatabaseDAO().getCar(carId)
-        }
-        return service.get()
+        return Single.create<CarData> {
+            val car = database.getCarDatabaseDAO().getCar(carId)
+            it.onSuccess(car)
+        }.subscribeOn(Schedulers.io())
+                .blockingGet()
     }
 
     fun updateCar(carData: CarData) {
-        executorService.submit { database.getCarDatabaseDAO().update(carData) }
+        Single.create<CarData> {
+            database.getCarDatabaseDAO().update(carData)
+        }.subscribeOn(Schedulers.io())
+                .subscribe()
     }
 
     /*****************************
@@ -50,28 +51,40 @@ class DatabaseRepository(context: Context) {
      *****************************/
 
     fun getParentWorks(parentCar: String?): List<WorkData> {
-        return executorService.submit<List<WorkData>> {
-            database.getWorkDatabaseDAO().getParentWorks(parentCar)
-        }.get()
+        return Single.create<List<WorkData>> {
+            val list = database.getWorkDatabaseDAO().getParentWorks(parentCar)
+            it.onSuccess(list)
+        }.subscribeOn(Schedulers.io())
+                .blockingGet()
     }
 
     fun getWork(workId: Long): WorkData {
-        val service = executorService.submit<WorkData> {
-            return@submit database.getWorkDatabaseDAO().getWork(workId)
-        }
-        return service.get()
+        return Single.create<WorkData> {
+            val work = database.getWorkDatabaseDAO().getWork(workId)
+            it.onSuccess(work)
+        }.subscribeOn(Schedulers.io())
+                .blockingGet()
     }
 
     fun updateWork(workData: WorkData) {
-        executorService.submit { database.getWorkDatabaseDAO().update(workData) }
+        Single.create<WorkData> {
+            database.getWorkDatabaseDAO().update(workData)
+        }.subscribeOn(Schedulers.io())
+                .subscribe()
     }
 
     fun deleteWork(workData: WorkData) {
-        executorService.submit { database.getWorkDatabaseDAO().delete(workData) }
+        Single.create<WorkData> {
+            database.getWorkDatabaseDAO().delete(workData)
+        }.subscribeOn(Schedulers.io())
+                .subscribe()
     }
 
     fun addWorkToDatabase(workData: WorkData) {
-        executorService.submit { database.getWorkDatabaseDAO().addWorkToDatabase(workData) }
+        Single.create<WorkData> {
+            database.getWorkDatabaseDAO().addWorkToDatabase(workData)
+        }.subscribeOn(Schedulers.io())
+                .subscribe()
     }
 
     /*********
